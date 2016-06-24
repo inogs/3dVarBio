7322c2e6b3f921560fb93c8e08e41aa87180a52e
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-06-24 16:55:22 +0200

    cleaning also chl and obs structures ;)

--- obs_chl.f90
+++ obs_chl.f90
@@ -1,63 +1,61 @@
 subroutine obs_chl
-
-!---------------------------------------------------------------------------
-!                                                                          !
-!    Copyright 2007 Srdjan Dobricic, CMCC, Bologna                         !
-!                                                                          !
-!    This file is part of OceanVar.                                          !
-!                                                                          !
-!    OceanVar is free software: you can redistribute it and/or modify.     !
-!    it under the terms of the GNU General Public License as published by  !
-!    the Free Software Foundation, either version 3 of the License, or     !
-!    (at your option) any later version.                                   !
-!                                                                          !
-!    OceanVar is distributed in the hope that it will be useful,           !
-!    but WITHOUT ANY WARRANTY; without even the implied warranty of        !
-!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         !
-!    GNU General Public License for more details.                          !
-!                                                                          !
-!    You should have received a copy of the GNU General Public License     !
-!    along with OceanVar.  If not, see <http://www.gnu.org/licenses/>.       !
-!                                                                          !
-!---------------------------------------------------------------------------
-
-!-----------------------------------------------------------------------
-!                                                                      !
-! Apply observational operator for chlorophyll                         !
-!                                                                      !
-!-----------------------------------------------------------------------
-
-
- use set_knd
- use grd_str
- use obs_str
-
- implicit none
-
- INTEGER(i4)   ::  i, j, k, l, kk
-
- do kk = 1,chl%no
-
-  if(chl%flc(kk).eq.1 )then
-
-
-    i=chl%ib(kk)
-    j=chl%jb(kk)
-
-     chl%inc(kk) = 0.0
-
-   do l=1,grd%nchl
-    do k=1,chl%kb(kk)
-     chl%inc(kk) = chl%inc(kk) + (                        &
-                   chl%pq1(kk) * grd%chl(i  ,j  ,k,l) +       &
-                   chl%pq2(kk) * grd%chl(i+1,j  ,k,l) +       &
-                   chl%pq3(kk) * grd%chl(i  ,j+1,k,l) +       &
-                   chl%pq4(kk) * grd%chl(i+1,j+1,k,l) ) * chl%dzr(k,kk)
-    enddo
-   enddo
-
-  endif
-
- enddo
-
+  
+  !---------------------------------------------------------------------------
+  !                                                                          !
+  !    Copyright 2007 Srdjan Dobricic, CMCC, Bologna                         !
+  !                                                                          !
+  !    This file is part of OceanVar.                                          !
+  !                                                                          !
+  !    OceanVar is free software: you can redistribute it and/or modify.     !
+  !    it under the terms of the GNU General Public License as published by  !
+  !    the Free Software Foundation, either version 3 of the License, or     !
+  !    (at your option) any later version.                                   !
+  !                                                                          !
+  !    OceanVar is distributed in the hope that it will be useful,           !
+  !    but WITHOUT ANY WARRANTY; without even the implied warranty of        !
+  !    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         !
+  !    GNU General Public License for more details.                          !
+  !                                                                          !
+  !    You should have received a copy of the GNU General Public License     !
+  !    along with OceanVar.  If not, see <http://www.gnu.org/licenses/>.       !
+  !                                                                          !
+  !---------------------------------------------------------------------------
+  
+  !-----------------------------------------------------------------------
+  !                                                                      !
+  ! Apply observational operator for chlorophyll                         !
+  !                                                                      !
+  !-----------------------------------------------------------------------
+  
+  
+  use set_knd
+  use grd_str
+  use obs_str
+  
+  implicit none
+  
+  INTEGER(i4)   ::  i, j, l, kk
+  
+  do kk = 1,chl%no
+     
+     if(chl%flc(kk).eq.1 )then
+        
+        
+        i=chl%ib(kk)
+        j=chl%jb(kk)
+        
+        chl%inc(kk) = 0.0
+        
+        do l=1,grd%nchl
+           chl%inc(kk) = chl%inc(kk) + (                        &
+                chl%pq1(kk) * grd%chl(i  ,j  ,1,l) +       &
+                chl%pq2(kk) * grd%chl(i+1,j  ,1,l) +       &
+                chl%pq3(kk) * grd%chl(i  ,j+1,1,l) +       &
+                chl%pq4(kk) * grd%chl(i+1,j+1,1,l) ) * chl%dzr(1,kk)
+        enddo
+        
+     endif
+     
+  enddo
+  
 end subroutine obs_chl
