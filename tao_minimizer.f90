3d7699ebc22764658944288dad0b8942c2f0e107
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-07-01 11:39:13 +0200

    minor changes

--- tao_minimizer.f90
+++ tao_minimizer.f90
@@ -9,10 +9,16 @@ subroutine tao_minimizer
   PetscErrorCode       ierr
   Tao                  tao
   
+  write(drv%dia,*) ''
+  write(drv%dia,*) 'call PetscInitialize'
   call PetscInitialize(PETSC_NULL_CHARACTER,ierr)
-  
+  write(drv%dia,*) 'PETSC init done :)'
+
   write(drv%dia,*) "Within tao_minimizer subroutine!"
-  
+   
+  write(drv%dia,*) 'call PetscFinalize'
   call PetscFinalize(ierr)
+  write(drv%dia,*) 'PetscFinalize done'
+  write(drv%dia,*) ''
 
 end subroutine tao_minimizer
