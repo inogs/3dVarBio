c983cc6f1ebc60048a4a206ddd27e159ce31de67
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-08-25 14:07:27 +0200

    adding Makefile and some other modifications

--- AllToAll/mytest.f90
+++ AllToAll/mytest.f90
@@ -5,8 +5,8 @@ program myalltoall
   implicit none
 
   integer :: MyRank, Size, ierr, i, j
-  integer :: GlobalRow, offset, localRow, localCol
-  real, allocatable :: Buffer(:,:), TmpBuf(:,:)
+  integer :: GlobalRow, localRow, localCol !, offset
+  real, allocatable :: Buffer(:,:), TmpBuf(:,:), RecBuf(:,:)
   
   call MPI_Init(ierr)
   call MPI_Comm_size(MPI_COMM_WORLD, Size, ierr)
@@ -15,31 +15,33 @@ program myalltoall
   write(*,*) "Hello world from process ", MyRank, " of ", Size
 
   localRow = 4
-  localCol = 4
+  localCol = 5 !4
   ALLOCATE(Buffer(localRow, localCol))
-  ALLOCATE(TmpBuf(localRow, localCol))
+  ! ALLOCATE(TmpBuf(localRow, localCol))
+  ALLOCATE(TmpBuf(localCol, localRow))
+  ALLOCATE(RecBuf(localRow, localCol))
 
-  GlobalRow = 8
-  offset = 4
-  
-  do i=1,localCol
-     do j=1,localRow
-        Buffer(i, j) = j + (i - 1) * GlobalRow + MyRank*offset
+  GlobalRow = 10 !8
+  ! offset = localCol !4
+
+  ! Store initial data
+  do i=1,localRow
+     do j=1,localCol
+        Buffer(i, j) = j + (i - 1) * GlobalRow + MyRank*localCol ! offset
      end do
   end do
   
-  ! do i=1,localCol
-  !    do j=1,localRow
-  !       Buffer(j, i) = j !+ (i - 1) * GlobalRow + MyRank*offset
-  !       TmpBuf(j, i) = 0.
-  !    end do
-  ! end do
+  do i=1,localCol
+     do j=1,localRow
+        TmpBuf(i,j) = Buffer(j,i)
+     end do
+  end do
 
-  TmpBuf(1,1) = 1.
-  
   write(*,*) "MyRank = ", MyRank
   print*, ""
   print*, Buffer
+  print*, ""
+  print*, TmpBuf
   print*, ""
   
   call MPI_Finalize(ierr)
