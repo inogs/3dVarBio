09c41c4af1db47de4d9d0f58827f73ee0b0dedfc
pdicerbo <pierluigi.dicerbo@gmail.com>
2017-01-30 11:55:23 +0100

    It works :)

--- DevUtils/OneSideComm.f90
+++ DevUtils/OneSideComm.f90
@@ -12,8 +12,6 @@ program OneSideComm
     real*8   :: Test
     
     real*8, pointer, dimension(:,:,:) :: a
-    ! real a
-    ! pointer (P, a(3,4,1))
 
     call MPI_Init(ierr)
     call MPI_Comm_rank(MPI_COMM_WORLD, MyRank, ierr)
@@ -30,7 +28,6 @@ program OneSideComm
     print*, "Hello World by", MyRank, "allocating matrix", im,"x",jm,"x",km
 
     ALLOCATE(a(im, jm, km))
-    ! call MPI_Alloc_mem(nbytes, MPI_INFO_NULL, P, ierr)
 
     do k=1,km
         do j=1,jm
@@ -41,18 +38,25 @@ program OneSideComm
     enddo
 
     call MPI_Win_create(a, nbytes, lenreal, MPI_INFO_NULL, MPI_COMM_WORLD, mpi_win_obj, ierr)
+    print*, MyRank, "exit from MPI_Win_create with ierr=", ierr
     call MPI_Win_fence(0, mpi_win_obj, ierr)
+    print*, MyRank, "exit from MPI_Win_fence with ierr=", ierr
 
     print*, "MyRank", MyRank, "print matrix:", a
     
     if(MyRank .eq. 0) then
-        target_displ = 0
-        CALL MPI_WIN_LOCK( MPI_LOCK_SHARED, 0, 0, mpi_win_obj, ierr )
+        target_displ = 2
+        CALL MPI_WIN_LOCK( MPI_LOCK_EXCLUSIVE, 1, 0, mpi_win_obj, ierr )
+        print*, MyRank, "exit from MPI_WIN_LOCK with ierr=", ierr
 
         call MPI_Get(Test, 1, MPI_REAL8, MyRank+1, target_displ, 1, MPI_REAL8, mpi_win_obj, ierr)
-        ! call MPI_Accumulate(a, MyCount, MPI_REAL8, 1, target_displ, MyCount, MPI_REAL8, MPI_SUM, mpi_win_obj, ierr)
+        print*, MyRank, "exit from MPI_Get with ierr=", ierr
+        target_displ = 0
+        call MPI_Accumulate(a, MyCount, MPI_REAL8, 1, target_displ, MyCount, MPI_REAL8, MPI_SUM, mpi_win_obj, ierr)
+        print*, MyRank, "exit from MPI_Accumulate with ierr=", ierr
         
-        call MPI_WIN_UNLOCK(0, mpi_win_obj, ierr)
+        call MPI_WIN_UNLOCK(1, mpi_win_obj, ierr)
+        print*, MyRank, "exit from MPI_WIN_UNLOCK with ierr=", ierr
         print*, ""
         print*, ""
         print*, ""
@@ -63,12 +67,13 @@ program OneSideComm
     endif
 
     call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    print*, MyRank, "exit from MPI_Barrier with ierr=", ierr
 
     print*, "MyRank", MyRank, "print matrix:", a
     
     call MPI_Win_free(mpi_win_obj, ierr)
+    print*, MyRank, "exit from MPI_Win_free with ierr=", ierr
 
-    ! call MPI_Free_mem(a, ierr)
     DEALLOCATE(a)
 
     call MPI_Finalize(ierr)
